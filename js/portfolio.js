1c68187b51580e98f4404fd20e03723ec216d18d
Jacqueline Linang <jacquelinang@JacquelinesMBP3.home>
2016-03-15 20:54:55 -0400

    Added portfolio section

---
+++ js/portfolio.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.portfolio').slick({
+	  infinite: true,
+	  dragability: true,
+	  rows: 1,
+	  slidesToShow: 1,
+	});
+});
--- js/slick.js
+++ js/slick.js
@@ -1,9 +1,22 @@
- $(document).ready(function(){
+$(document).ready(function(){
+var w = window.innerWidth;
+
+	if(w < 500)
+	{
+		$('.row').slick({
+		  infinite: true,
+		  dragability: true,
+		  rows: 1,
+		  slidesToShow: 1,
+		});
+
+	}
+
+	else
 	$('.row').slick({
-		infinite: true,
+	  infinite: true,
 	  dragability: true,
 	  rows: 1,
 	  slidesToShow: 2,
 	});
-				
 });
